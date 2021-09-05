663a2dc471c4b3cddccd884b847d882f7678292c
Yuvati mankar <yuvatimankar06@gmail.com>
2021-09-05 18:24:58 +0530

    adding toh

---
+++ TOH.cpp
@@ -0,0 +1,28 @@
+//Tower of hanoi
+#include<iostream>
+
+using namespace std;
+
+void TOH(int n,int A,int B,int C)
+{
+    if(n>0)
+    {
+        TOH(n-1,A,C,B);
+        cout<<A<<","<<C<<endl;
+        TOH(n-1,B,A,C);
+
+
+    }
+}
+int main()
+{
+    int n;
+    cin>>n; 
+    
+    int a,b,c;
+    cin>>a>>b>>c;
+    TOH(n,a,b,c);
+
+    
+    return 0;
+}
